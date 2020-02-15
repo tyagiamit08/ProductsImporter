9fc4ab6e2ecbbf833cacd77326092f873947860a
Amit Tyagi <amit.tyagi@external.mckinsey.com>
2020-02-16 00:00:18 +0530

    Added ProductsImporter Factory

---
+++ src/ProductsImporter.BL/Factory/ProductsImporterFactory.cs
@@ -0,0 +1,27 @@
+﻿using ProductsImporter.BL.Constants;
+using ProductsImporter.BL.Implementation;
+using ProductsImporter.BL.Interfaces;
+
+namespace ProductsImporter.BL.Factory
+{
+	public class ProductsImporterFactory
+	{
+		private readonly IStreamReader _streamReader;
+		public ProductsImporterFactory(IStreamReader streamReader)
+		{
+			_streamReader = streamReader;
+		}
+
+		public IProductsImporter ResolveImporter(string productsProvider, string fileExtension)
+		{
+			return productsProvider.ToLower() switch
+			{
+				ProductsProviders.SoftwareAdvice when fileExtension == FileExtensions.Json =>
+				new SoftwareAdviceProductsImporter(_streamReader),
+				ProductsProviders.Capterra when fileExtension == FileExtensions.Yaml => 
+				new CapterraProductsImporter(_streamReader),
+				_ => null
+			};
+		}
+	}
+}
