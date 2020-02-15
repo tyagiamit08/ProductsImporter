b10260b9f5d92463ac0af6b755fd7ac3e5ea490a
Amit Tyagi <amit.tyagi@external.mckinsey.com>
2020-02-16 00:21:14 +0530

    Added ProductsImporter.BL.Tests project for Unit and Functional tests

---
+++ src/ProductsImporter.BL.Tests/FunctionalTest/SoftwareAdviceProductsImporterTest.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FluentAssertions;
+using Moq;
+using ProductsImporter.BL.Implementation;
+using ProductsImporter.BL.Interfaces;
+
+namespace TestProject.Bl.Test
+{
+	[TestFixture]
+	public class SoftwareAdviceProductsImporterTest
+	{
+		private SoftwareAdviceProductsImporter _softwareAdviceProductsImporter;
+		private Mock<IStreamReader> _mockStreamReader;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mockStreamReader = new Mock<IStreamReader>();
+			_softwareAdviceProductsImporter = new SoftwareAdviceProductsImporter(_mockStreamReader.Object);
+		}
+
+		[Test(Description = "Given valid filePath with valid products then it")]
+		public void Should_Return_Valid_List_Of_Products()
+		{
+			const string dummyProductsJson = @"{""products"": [{""categories"": [""Customer Service"",""Call Center""],""twitter"": ""@freshdesk"",""title"": ""Freshdesk""},{""categories"": [""CRM"",""Sales Management""],""title"": ""Zoho""}]}";
+
+			var byteArray = Encoding.ASCII.GetBytes(dummyProductsJson);
+			var stream = new MemoryStream(byteArray);
+
+			_mockStreamReader.Setup(m => m.GetReader(It.IsAny<string>())).Returns(new StreamReader(stream));
+
+			var products = _softwareAdviceProductsImporter.Import("foo");
+
+			_mockStreamReader.Verify(m => m.GetReader(It.IsAny<string>()), Times.Once);
+
+			products.Should().NotBeNull();
+			products.Count.Should().Be(2);
+
+			products.First().Name.Should().BeEquivalentTo("\"Freshdesk\"");
+			products.First().Twitter.Should().BeEquivalentTo("@freshdesk");
+			products.First().Categories[0].Should().BeEquivalentTo("Customer Service");
+			products.First().Categories[1].Should().BeEquivalentTo("Call Center");
+		}
+
+		[Test(Description = "Given filePath which is not valid or products not found in the file then it")]
+		public void Should_Return_Empty_List_Of_Products()
+		{
+			const string dummyProductsJson = @"{""products"":[]}";
+
+			var byteArray = Encoding.ASCII.GetBytes(dummyProductsJson);
+			var stream = new MemoryStream(byteArray);
+
+			_mockStreamReader.Setup(m => m.GetReader(It.IsAny<string>())).Returns(new StreamReader(stream));
+
+			var products = _softwareAdviceProductsImporter.Import("foo");
+
+			_mockStreamReader.Verify(m => m.GetReader(It.IsAny<string>()), Times.Once);
+
+			products.Should().NotBeNull();
+			products.Count.Should().Be(0);
+		}
+	}
+}
---
+++ src/ProductsImporter.BL.Tests/UnitTest/ProductsImporterFactoryTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using ProductsImporter.BL.Factory;
+using ProductsImporter.BL.Implementation;
+using ProductsImporter.BL.Interfaces;
+
+namespace TestProject.Bl.Test
+{
+	public class ProductsImporterFactoryTest
+	{
+		private ProductsImporterFactory _productsImporterFactory;
+		private Mock<IStreamReader> _mockStreamReader;
+
+		[SetUp]
+		public void Setup()
+		{
+			_mockStreamReader = new Mock<IStreamReader>();
+			_productsImporterFactory = new ProductsImporterFactory(_mockStreamReader.Object);
+		}
+
+		[Test(Description = "When products importer is capterra and fileExtension is .yaml then it")]
+		public void Should_Return_Object_Of_Capterra_Products_Importer()
+		{
+			const string productsImporter = "capterra";
+			const string fileExtension = ".yaml";
+
+			var result = _productsImporterFactory.ResolveImporter(productsImporter, fileExtension);
+
+			result.Should().NotBeNull();
+			result.Should().BeOfType<CapterraProductsImporter>();
+		}
+
+		[Test(Description = "When products importer is softwareAdvice and fileExtension is .json then it")]
+		public void Should_Return_Object_Of_SoftwareAdvice_Products_Importer()
+		{
+			const string productsImporter = "softwareAdvice";
+			const string fileExtension = ".json";
+
+			var result = _productsImporterFactory.ResolveImporter(productsImporter, fileExtension);
+
+			result.Should().NotBeNull();
+			result.Should().BeOfType<SoftwareAdviceProductsImporter>();
+		}
+
+
+		[Test(Description = "When products importer is neither capterra nor softwareAdvice then it")]
+		public void Should_Return_Null()
+		{
+			const string productsImporter = "foo";
+			const string fileExtension = ".bar";
+
+			var result = _productsImporterFactory.ResolveImporter(productsImporter, fileExtension);
+
+			result.Should().BeNull();
+		}
+	}
+}
