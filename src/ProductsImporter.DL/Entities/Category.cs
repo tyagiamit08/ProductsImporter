c9f541100f264e5e8d460ea3a1ea2db3f6f0a8ee
Amit Tyagi <amit.tyagi@external.mckinsey.com>
2020-02-16 00:23:57 +0530

    Added ProductsImporter.DL project with the DB Entities

---
+++ src/ProductsImporter.DL/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsImporter.DL.Entities
+{
+	public class Category
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public Product Product { get; set; }
+	}
+}
---
+++ src/ProductsImporter.DL/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ProductsImporter.DL.Entities
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Twitter { get; set; }
+		public ICollection<Category> Categories { get; set; }
+	}
+}
