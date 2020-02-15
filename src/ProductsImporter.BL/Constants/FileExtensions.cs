ae9067a90d0f0d11affce70491d2d66b367542da
Amit Tyagi <amit.tyagi@external.mckinsey.com>
2020-02-15 23:40:33 +0530

    Added projects for ProductsImporter Console App and ProductsImporter BL

---
+++ src/ProductsImporter.BL/Constants/FileExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductsImporter.BL.Constants
+{
+	public class FileExtensions
+	{
+		public const string Json = ".json";
+		public const string Yaml = ".yaml";
+	}
+}
---
+++ src/ProductsImporter.BL/Constants/ProductsProviders.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductsImporter.BL.Constants
+{
+	public class ProductsProviders
+	{
+		public const string Capterra = "capterra";
+		public const string SoftwareAdvice = "softwareadvice";
+	}
+}
